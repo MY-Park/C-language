9675ed42c4b0d1e449f41a5f795c9dff2171bcdf
CG-term.git <mypark109@naver.com>
2018-03-13 18:04:05 +0900

    add tab_to_blank

---
+++ tab_to_blank.c
@@ -0,0 +1,39 @@
+//
+//  tab_to_blank.c
+//  Test01
+//
+//  Created by 박민영 on 13/03/2018.
+//  Copyright © 2018 박민영. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(){
+    char ch;
+    int cnt = 0;
+    int quo = 0;
+    int mul = 0;
+    
+    while((ch=getchar())!=EOF){
+        if(ch == '\n'){
+            cnt = 0;
+            putchar(ch);
+            continue;
+        }
+        
+        if(ch != '\t'){
+            cnt++;
+            putchar(ch);
+        }else{
+            quo = cnt / 4;
+            mul = (quo + 1) * 4;
+            for(; cnt < mul; cnt++){
+                putchar(' ');
+            }
+            
+        }
+    }
+    return 0;
+}
+
+
