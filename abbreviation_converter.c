8ace95bf1c96161a4d2a89178ce6d507a57ec53c
CG-term.git <mypark109@naver.com>
2018-03-15 17:57:52 +0900

    abbreviation_converter

--- abbreviation_converter.c
+++ abbreviation_converter.c
@@ -12,6 +12,34 @@
 #define SIZE 1024
 
 int main(){
-
+    char str[SIZE][SIZE];
+    int len, i;
+    int check;
+    int idx = 0;
+    
+    while(fgets(str[idx], SIZE, stdin)!=NULL){
+        idx++;
+    }
+    for(int k=0; k<idx; k++){
+        len = (int)strlen(str[k]);
+        for(i=0; i<len-2; i++){
+            check = 0;
+            if(str[k][i] >= 'A' && str[k][i] <= 'Z' && str[k][i+1] == '-' && str[k][i+2] >= str[k][i] && str[k][i+2] <= 'Z'){
+                check = 1;
+            }else if(str[k][i] >= '0' && str[k][i] <= '9' && str[k][i+1] == '-' && str[k][i+2] >= str[k][i] && str[k][i+2] <= '9'){
+                check = 1;
+            }else if(str[k][i] >= 'a' && str[k][i] <= 'z' && str[k][i+1] == '-' && str[k][i+2] >= str[k][i] && str[k][i+2] <= 'z'){
+                check = 1;
+            }
+            
+            if(check){
+                for(char j=str[k][i]; j<=str[k][i+2]; j++) putchar(j);
+                i = i + 2;
+            }else{
+                putchar(str[k][i]);
+            }
+        }
+        for(;i<len;i++) putchar(str[k][i]);
+    }
     return 0;
 }
